d4c5e509836d718c63fd98b684ae40e1d2d75786
ddiu8081 <ddiu8081@gmail.com>
2023-02-18 20:56:01 +0800

    chore: git init

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,10 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+import vercel from "@astrojs/vercel/serverless";
+
+// https://astro.build/config
+export default defineConfig({
+  output: "server",
+  adapter: vercel()
+});
