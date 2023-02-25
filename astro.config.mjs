87be3b2d8ec8b289c26789f96a9958e4cc2b4560
ddiu8081 <ddiu8081@gmail.com>
2023-02-25 21:25:09 +0800

    feat: base ui

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,19 @@
-import { defineConfig } from 'astro/config';
-
-// https://astro.build/config
-import vercel from "@astrojs/vercel/serverless";
+import { defineConfig } from 'astro/config'
+import vercel from '@astrojs/vercel/serverless'
+import unocss from 'unocss/astro'
+import { presetUno } from 'unocss'
+import presetAttributify from '@unocss/preset-attributify'
 
 // https://astro.build/config
 export default defineConfig({
-  output: "server",
+  integrations: [
+    unocss({
+      presets: [
+        presetAttributify(),
+        presetUno(),
+      ],
+    }),
+  ],
+  output: 'server',
   adapter: vercel()
 });
